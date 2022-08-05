e7ade8ba5cae4dda9965037825853fc68e1631c2
Josue Pujols (ClaroDom) <josue_pujols@claro.com.do>
2022-08-04 23:12:43 -0400

    New controller Entidades created

---
+++ SellPoint/SellPoint.Presentation.API/Controllers/EntidadesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SellPoint.Presentation.API.Controllers
+{
+    public class EntidadesController : Controller
+    {
+        public EntidadesController()
+        {
+
+        }
+    }
+}
