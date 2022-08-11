0bcf5cbdc461401c6b23d913bbb04a61602edfe6
Josue Pujols (ClaroDom) <josue_pujols@claro.com.do>
2022-08-11 13:07:07 -0400

    Login navigation finished, just fix the styles of the page

--- SellPoint/SellPoint.Presentation.WebClient/Program.cs
+++ SellPoint/SellPoint.Presentation.WebClient/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SellPoint.Presentation.WebClient;
+using SellPoint.Presentation.WebClient.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7131") });
+builder.Services.AddScoped(x => new StateContainer());
 
 await builder.Build().RunAsync();
