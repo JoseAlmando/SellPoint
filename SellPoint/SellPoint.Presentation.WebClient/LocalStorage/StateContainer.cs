45bdaa45b30ef63cee38716113e8d411c7a38932
Josue Pujols (ClaroDom) <josue_pujols@claro.com.do>
2022-08-13 13:06:02 -0400

    change entitity crud in client and trying to reload when add/edit

--- SellPoint/SellPoint.Presentation.WebClient/LocalStorage/StateContainer.cs
+++ SellPoint/SellPoint.Presentation.WebClient/LocalStorage/StateContainer.cs
@@ -5,6 +5,17 @@ namespace SellPoint.Presentation.WebClient.LocalStorage
     public class StateContainer
     {
         private User? savedUser;
+        private List<Entidades> entidades;
+
+        public List<Entidades> Entidades
+        {
+            get => entidades ?? new List<Entidades>();
+            set
+            {
+                entidades = value;
+                NotifyStateChanged();
+            }
+        }
 
         public User user
         {
